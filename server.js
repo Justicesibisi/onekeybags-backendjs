b47d4a8cdf4767a703792dc2be5a0315c7ee6f14
Justicesibisi <djmangsa@gmail.com>
2024-07-10 23:13:02 +0200

    test server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemon = require('nodemon');
 const app = express();
 const router = express.Router();
 
@@ -28,3 +29,21 @@ app.use('/api', router);
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+// Use nodemon for automatic server restarts during development
+if (process.env.NODE_ENV !== 'production') {
+  nodemon({
+    script: src/server.js,
+    ext: 'js',
+    ignore: ['node_modules/']
+  });
+  nodemon.on('start', () => {
+    console.log('Server has started');
+  });
+  nodemon.on('restart', () => {
+    console.log('Server has restarted');
+  });
+  nodemon.on('quit', () => {
+    console.log('Server has stopped');
+    process.exit();
+  });
+}
